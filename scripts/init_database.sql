4936f294b354796a732a33d01e2eab49e9726b9f
Gptts12 <gene.potts@hotmail.com>
2026-02-04 19:43:17 -0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,20 @@
+
+
+/* 
+Scripts for new Data Warehouse
+*/
+
+-- Create Database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DATAWAREHOUSE;
+
+USE DATAWAREHOUSE;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
